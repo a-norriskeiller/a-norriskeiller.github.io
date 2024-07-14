38fe806c93717b1101fdba27d6ad759eb8a845ec
a-norriskeiller <71377341+a-norriskeiller@users.noreply.github.com>
2024-07-14 13:52:47 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,40 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     const photos = [
-        //'images/pondweed1.jpg',
-        //'images/pondweed2.jpg',
         'images/clouds.jpg',
         'images/shadow.jpg'
         // Add more photo paths as needed
     ];
 
-    function getRandomPhoto() {
-        const randomIndex = Math.floor(Math.random() * photos.length);
-        return photos[randomIndex];
-    }
-
-    function showRandomPhoto() {
-        const randomPhoto = getRandomPhoto();
-        const randomPhotoElement = document.getElementById('random-photo');
-        randomPhotoElement.src = randomPhoto;
-        randomPhotoElement.style.display = 'block'; // Ensure the photo is displayed
-    }
-
-    // Show random photo on initial load and when the home link is clicked
-    document.getElementById('home-link').addEventListener('click', function () {
-        showRandomPhoto();
-        showSection('home');
-    });
+    const randomPhoto = photos[Math.floor(Math.random() * photos.length)];
+    const randomPhotoElement = document.getElementById('random-photo');
+    randomPhotoElement.src = randomPhoto;
+    randomPhotoElement.alt = 'Random Photo';
 
     const abstractLinks = document.querySelectorAll('.abstract-link');
+
     abstractLinks.forEach(link => {
         link.addEventListener('click', function (event) {
             event.preventDefault();
             const abstractContent = this.parentElement.nextElementSibling;
-            if (abstractContent.style.display === 'block') {
-                abstractContent.style.display = 'none';
-            } else {
-                document.querySelectorAll('.abstract-content').forEach(content => content.style.display = 'none');
+            if (abstractContent.style.display === 'none' || abstractContent.style.display === '') {
                 abstractContent.style.display = 'block';
+            } else {
+                abstractContent.style.display = 'none';
             }
         });
     });
@@ -46,23 +31,15 @@ document.addEventListener('DOMContentLoaded', function () {
             const targetSection = document.getElementById(this.dataset.target);
             document.querySelectorAll('.research-section').forEach(section => section.style.display = 'none');
             targetSection.style.display = 'block';
-
-            // Fade in the new section
-            const contentSections = document.querySelectorAll('.content-section');
-            contentSections.forEach(section => {
-                section.style.display = 'none';
-                section.classList.remove('fade-in');
-            });
-            document.querySelector('.right-column').classList.remove('blacked-out');
         });
     });
 
+    // Reveal quote
     const quoteLink = document.querySelector('.reveal-quote');
     quoteLink.addEventListener('click', function (event) {
         event.preventDefault();
         const quoteDisplay = document.getElementById('quote-display');
         quoteDisplay.style.display = 'block';
-        quoteDisplay.classList.add('fade-in'); // Add fade-in class
         const rightColumn = document.querySelector('.right-column');
         rightColumn.classList.add('blacked-out');
         setTimeout(() => {
@@ -71,36 +48,26 @@ document.addEventListener('DOMContentLoaded', function () {
         }, 5000);
     });
 
+    // Handle navigation links click
     const navLinks = document.querySelectorAll('.nav-link');
     navLinks.forEach(link => {
         link.addEventListener('click', function (event) {
             event.preventDefault();
 
+            if (this.id === 'home-link') {
+                location.reload(); // Reload the page
+                return;
+            }
+
             const targetSection = document.getElementById(this.dataset.target);
+            const photoContainer = document.getElementById('photo-container');
+            photoContainer.style.display = 'none';
             document.querySelectorAll('.content-section').forEach(section => {
                 section.style.display = 'none';
                 section.classList.remove('fade-in');
             });
             targetSection.style.display = 'block';
             targetSection.classList.add('fade-in');
-
-            // Hide the photo
-            const photoContainer = document.getElementById('photo-container');
-            photoContainer.style.display = 'none';
         });
     });
-
-    // Initial random photo load
-    showRandomPhoto();
-
-    function showSection(sectionId) {
-        const sections = document.querySelectorAll('.content-section');
-        sections.forEach(section => {
-            if (section.id === sectionId) {
-                section.style.display = 'block';
-            } else {
-                section.style.display = 'none';
-            }
-        });
-    }
 });
