21c8d09c433ae9c54a04838d3032aea29168977a
a-norriskeiller <71377341+a-norriskeiller@users.noreply.github.com>
2024-07-21 14:11:14 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const researchLink = document.getElementById("research-link");
     const cvLink = document.getElementById("cv-link");
     const quoteLink = document.getElementById("quote-link");
+    const homeLinkText = document.getElementById("home-link-text");
     const researchContent = document.getElementById("research-content");
     const quoteContent = document.getElementById("quote-content");
     const rightPanel = document.getElementById("right-panel");
@@ -29,11 +30,22 @@ document.addEventListener("DOMContentLoaded", function () {
         randomPhoto.classList.add("hidden");
         researchContent.classList.add("hidden");
         quoteContent.classList.remove("hidden");
+        quoteContent.classList.add("fade-in");
+    }
+
+    function hideQuoteContent() {
+        quoteContent.classList.remove("fade-in");
+        quoteContent.classList.add("fade-out");
+        setTimeout(() => {
+            quoteContent.classList.add("hidden");
+            quoteContent.classList.remove("fade-out");
+        }, 1000); // Adjust the timing to match the fade-out animation
     }
 
     researchLink.addEventListener("click", function (event) {
         event.preventDefault();
-        showResearchContent();
+        hideQuoteContent();
+        setTimeout(showResearchContent, 1000); // Adjust the timing to match the fade-out animation
     });
 
     cvLink.addEventListener("click", function (event) {
@@ -42,15 +54,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
     quoteLink.addEventListener("click", function (event) {
         event.preventDefault();
-        showQuoteContent();
+        hideQuoteContent();
+        setTimeout(showQuoteContent, 1000); // Adjust the timing to match the fade-out animation
+    });
+
+    homeLinkText.addEventListener("click", function (event) {
+        event.preventDefault();
+        loadRandomPhoto();
+        researchContent.classList.add("hidden");
+        quoteContent.classList.add("hidden");
     });
 
     loadRandomPhoto(); // Load a random photo on page load
 
     document.querySelectorAll('.subheading').forEach(subheading => {
         subheading.addEventListener('click', function () {
+            document.querySelectorAll('.entry').forEach(entry => entry.classList.add('hidden'));
             const target = document.getElementById(subheading.dataset.target);
-            target.classList.toggle('hidden');
+            target.classList.remove('hidden');
         });
     });
 
