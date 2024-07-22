2bdf82a98d012365f2d8ef483461f3bfe27072e9
a-norriskeiller <71377341+a-norriskeiller@users.noreply.github.com>
2024-07-22 12:51:21 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -75,11 +75,17 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    document.querySelectorAll('.abstract-link').forEach(link => {
+    const abstractLinks = document.querySelectorAll('.abstract-link');
+
+    abstractLinks.forEach(link => {
         link.addEventListener('click', function (event) {
             event.preventDefault();
-            const abstract = this.nextElementSibling;
-            abstract.classList.toggle('hidden');
+            const abstractContent = this.parentElement.nextElementSibling;
+            if (abstractContent.style.display === 'none' || abstractContent.style.display === '') {
+                abstractContent.style.display = 'block';
+            } else {
+                abstractContent.style.display = 'none';
+            }
         });
     });
 });
